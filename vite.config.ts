aacc6591cd26749b2e616ab56eeb60d22f58fed4
ui1221 <ui1221@users.noreply.github.com>
2026-05-13 01:12:10 +0900

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/machine-defense/',
   server: { port: parseInt(process.env.PORT || '5173') },
   build: { target: 'es2020' }
 })
